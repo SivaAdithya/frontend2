ad8adddbd034ab73389405ff5de353264cecd686
SivaAdithya <sivamj14@gmail.com>
2023-07-13 22:02:27 +0530

    first commit

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -17,7 +17,7 @@ function Signup() {
   e.preventDefault();
 
   try {
-    const response = await axios.post("https://server1-two.vercel.app/backend/signup/", {
+    const response = await axios.post("https://server3-rho.vercel.app/backend/signup/", {
       name,
       email,
       password,
